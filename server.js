48535072cf6cd3e6ca950c2a2bcf2afc90b8e807
Darobinson20 <Darobinson20@gmail.com>
2022-07-13 00:10:07 -0400

    added gitignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path')
 const express = require('express');
+const apiRoutes = require('./public/')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use()
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
