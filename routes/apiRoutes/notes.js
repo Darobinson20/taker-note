c2dcb77e241552474e8e08338b03c89f57c7ad8b
Darobinson20 <Darobinson20@gmail.com>
2022-07-13 01:48:10 -0400

    routes started

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results)
+});
+
+router.post('/notes', (req, res) => {
+    st
+    
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-const path = require('path')
 const express = require('express');
-const apiRoutes = require('./public/')
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes/notes');
+const htmlRoutes = require('./routes/htmlRoutes/index');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api', )
-app.use('/')
+// app.use('/api', apiRoutes);
+// app.use('/',htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
