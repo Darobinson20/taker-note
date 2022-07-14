65025b59e74e9355ddc56cf9ababfd99c9812170
Darobinson20 <Darobinson20@gmail.com>
2022-07-14 00:19:53 -0400

    Add Heroku

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,25 +1,25 @@
-const router = require('express').Router();
-const notes = require('../../db/db.json');
-const fs = require('fs');
+const router = require("express").Router();
+const notes = require("../../db/db.json");
+const fs = require("fs");
 
- 
-const data = fs.readFileSync('./db/db.json', {encoding: 'utf8'});
+const data = fs.readFileSync("./db/db.json", { encoding: "utf8" });
 
-
-
-
-
-router.get('/notes', (req, res) => {
-    let results = notes;
-    res.json(results)
+router.get("/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
 });
 
-router.post('./notes', (req, res) => {
-  store.addNote().then((data) => {
-    res.json(data)
-  })
-  .catch((err) => res.status(404).json(err))
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  // req.body.id = notes.length.toString();
+
+  notes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(data);
 });
+// .catch((err) => res.status(404).json(err));
 
 // notes();
 
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes/notes');
-const htmlRoutes = require('./routes/htmlRoutes/index');
-
+const apiRoutes = require("./routes/apiRoutes/notes");
+const htmlRoutes = require("./routes/htmlRoutes/index");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/api', apiRoutes);
- app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
+  console.log(`API server now on port ${PORT}`);
 });
