41487bf21d4020d091db502325ddf015e8dbfcb9
Darobinson20 <Darobinson20@gmail.com>
2022-07-13 22:00:47 -0400

    almost there - I think

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
+const { notes } = require()
+
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results)
 });
 
-router.post('/notes', (req, res) => {
-    st
-    
-})
+router.post('./notes', (req, res) => {
+  store.addNote().then((data) => {
+    res.json(data)
+  })
+  .catch((err) => res.status(404).json(err))
+});
 
 module.exports = router;
